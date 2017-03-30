65f7ee0e963fe42c26540429615f48224d321259
abakarmagomedov <skat131998@mail.ru>
2017-03-30 17:37:27 +0300

    initializing greenDao in SplashActivity

--- app/src/main/java/com/asuper/abocha/cs_go/SplashActivity.java
+++ app/src/main/java/com/asuper/abocha/cs_go/SplashActivity.java
@@ -3,11 +3,27 @@ package com.asuper.abocha.cs_go;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import com.asuper.abocha.cs_go.Map.MapActivity;
+import com.asuper.abocha.cs_go.Model.GameMap;
+
+import java.util.Map;
+
+public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        if(gameMapDao.loadAll().size() > 0) {
+            start(MapActivity.class);
+        }else {
+
+            GameMap gameMap = new GameMap("De dust 2", "this is mGameMap", R.drawable.de_inferno, "", "", "");
+            GameMap gameMap2 = new GameMap("De dust 2", "this is mGameMap", R.drawable.de_inferno, "", "", "");
+            GameMap gameMap3 = new GameMap("De dust 2", "this is mGameMap", R.drawable.de_inferno, "", "", "");
+            gameMapDao.insertOrReplaceInTx(gameMap, gameMap2, gameMap3);
+            start(MapActivity.class);
+        }
     }
 }
