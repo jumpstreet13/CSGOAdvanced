d0aecf0f81a63ff56879d73d00e59e8047e34134
abakarmagomedov <skat131998@mail.ru>
2017-04-14 17:56:44 +0300

    appearing of bottomsheet

--- app/src/main/java/com/asuper/abocha/cs_go/Stream/StreamActivity.java
+++ app/src/main/java/com/asuper/abocha/cs_go/Stream/StreamActivity.java
@@ -2,8 +2,10 @@ package com.asuper.abocha.cs_go.Stream;
 
 import android.app.ProgressDialog;
 import android.net.Uri;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.asuper.abocha.cs_go.App;
 import com.asuper.abocha.cs_go.BaseActivity;
@@ -12,6 +14,7 @@ import com.asuper.abocha.cs_go.R;
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import io.vov.vitamio.MediaPlayer;
 import io.vov.vitamio.widget.MediaController;
 import io.vov.vitamio.widget.VideoView;
@@ -20,18 +23,26 @@ public class StreamActivity extends BaseActivity {
 
 
     @Inject ProgressDialog mProgressDialog;
+    @BindView(R.id.bottom_sheet) View bottomSheet;
     @BindView(R.id.videoView) VideoView videoView;
+    private BottomSheetBehavior behaviour;
+
+    @OnClick(R.id.activity_stream_coordinator_layout)
+    void onLayoutClick(){
+        behaviour.setState(BottomSheetBehavior.STATE_EXPANDED);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stream);
+        behaviour = BottomSheetBehavior.from(bottomSheet);
         mProgressDialog.show();
         String httpLiveUrl = "";  // TODO: 14.04.17 https://www.reddit.com/r/DotA2/comments/3dy316/small_website_to_extract_raw_stream_urls_from/ where i can get link to stream
         videoView.setVideoURI(Uri.parse(httpLiveUrl));
         MediaController mediaController = new MediaController(this);
         videoView.setMediaController(mediaController);
-        videoView.requestFocus();
+        videoView.requestFocus(); // TODO: 14.04.17 Refactor this please
         videoView.setVideoChroma(MediaPlayer.VIDEOCHROMA_RGB565);
         videoView.setVideoQuality(MediaPlayer.VIDEOQUALITY_MEDIUM);
         videoView.start();
@@ -41,6 +52,8 @@ public class StreamActivity extends BaseActivity {
                 break;
             }
         }
+
+        // TODO: 14.04.17 Maybe SnackBar instead of bottomsheet
 
         // TODO: 14.04.17 Add slide transition appear http://stackoverflow.com/questions/10243557/how-to-apply-slide-animation-between-two-activities-in-android
     }
